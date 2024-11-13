bf56c1f9109754980d2c9b280c575e010f9e6bb2
Lucas Pinheiro <pinheiros.dev@gmail.com>
2024-11-12 22:09:52 -0300

    Delete codeAula/pokedex-app directory

--- codeAula/pokedex-app/routes/pokemonRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pokemonController = require('../controllers/pokemonController');
-
-router.get('/', pokemonController.getAllPokemons);
-router.get('/pokemon/:id', pokemonController.getPokemon);
-
-module.exports = router;
